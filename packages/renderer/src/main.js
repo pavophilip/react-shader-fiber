d78f934071e1d4d96735db3184e4f5e236653677
pavophilip <pavophilip@gmail.com>
2023-12-23 17:44:51 +0100

    fix: update exports

--- packages/renderer/src/main.js
+++ packages/renderer/src/main.js
@@ -1,10 +1,3 @@
-import render, { testRender } from "./render.js";
+export * from "./render.js";
 export const Program = "Program";
 export const Ast = "Ast";
-
-export default {
-  render,
-  testRender,
-  Ast,
-  Program,
-};
