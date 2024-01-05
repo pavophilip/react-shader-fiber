e182ac7f6bf8b6a8fe420cb9c129134c882225a5
pavophilip <pavophilip@gmail.com>
2024-01-05 21:43:45 +0100

    fix: docs config

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://pavophilip.github.io/react-shader-fiber/",
+  url: "https://pavophilip.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/react-shader-fiber",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
